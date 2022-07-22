15d0eb70c8453f1eac43ae108442c49e83ac447d
Charlotte Marion <marion.charlotte@orange.fr>
2022-07-22 09:55:48 +0200

    modification pour mettre element en classe interne

--- src/fr/m2i/ListeDChainee.java
+++ src/fr/m2i/ListeDChainee.java
@@ -110,5 +110,45 @@ public class ListeDChainee<T> {
 	public void setDernierElement(Element<T> dernierElement) {
 		this.dernierElement = dernierElement;
 	}
+	
+	
+	
+	@SuppressWarnings("hiding")
+	private class Element <T> {
+
+		private T valeurElement;
+		private Element <T> precElement;
+		private Element <T> suivElement;
+		
+		public Element(T valeurElement) {
+			this.setValeurElement(valeurElement);
+		}
+		
+		public Element(T valeurElement, Element <T> precElement) {
+			this.setValeurElement(valeurElement);
+			this.setPrecElement(precElement);
+		}
+		
+		public T getValeurElement() {
+			return valeurElement;
+		}
+		public void setValeurElement(T valeurElement) {
+			this.valeurElement = valeurElement;
+		}
+		public Element <T> getPrecElement() {
+			return precElement;
+		}
+		public void setPrecElement(Element <T> precElement) {
+			this.precElement = precElement;
+		}
+		public Element <T> getSuivElement() {
+			return suivElement;
+		}
+		public void setSuivElement(Element <T> suivElement) {
+			this.suivElement = suivElement;
+		}
+		
+	}
+
 
 }
